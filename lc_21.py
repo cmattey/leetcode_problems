d4d9aa58e91d2e72e3462d438c22f287cd77b713
cmattey <cmattey@dons.usfca.edu>
2019-07-02 22:23:51 -0700

    Added LC21 Merge Two Sorted Lists Solution

---
+++ lc_21.py
@@ -0,0 +1,25 @@
+# 21. Merge Two Sorted Lists
+# Time: O(len(l1)+len(l2))
+# Space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+
+        while l1 and l2:
+            if l1.val<l2.val:
+                l1.next = self.mergeTwoLists(l1.next,l2)
+                return l1
+            else:
+                l2.next = self.mergeTwoLists(l1,l2.next)
+                return l2
