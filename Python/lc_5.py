a69f63fc2cdce94ceac2e5ce3942486b7cbaafb2
cmattey <cmattey@dons.usfca.edu>
2019-08-11 23:52:15 -0700

    Added comments about filling DP matrix

--- Python/lc_5.py
+++ Python/lc_5.py
@@ -2,33 +2,37 @@
 # Time: O(n^2)
 # Space: O(n^2)
 
-# An element of the dpmat[i][j], reprents whether s[i:j+1], is a palindrome or not.
-# The base cases are:
-# dpmat[i][j] = True, if i==j
-# dpmat[i][j] = True, if i+1==j and s[i]==s[j]
-# the dp relation then is: dpmat[i][j] = True if dpmat[i+1][j-1]==True and s[i]==s[j]
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
-        dpmat = [[False for i in range(len(s))] for i in range(len(s))]
+        st_mat = [[False for _ in range(len(s))] for _ in range(len(s))]
 
+        for i in range(len(s)):
+            st_mat[i][i]=True
+
+            if i+1 in range(len(s)):
+                if s[i]==s[i+1]:
+                    st_mat[i][i+1]=True
+
+        st_mat[i][j]=True if st_mat[i+1][j-1]==True and s[i]==s[j]
+
+
+        # Order of filling up the matrix is important!
+        # We are essentially considering all combinations in this order:
+        # [n-2..n-1], [n-3..n-1], [n-4..n-1], [0..n-1]
+        for start in range(len(s)-2,-1,-1):
+            for end in range(start+1, len(s)):
+
+                if st_mat[start+1][end-1] and s[start]==s[end]:
+                    st_mat[start][end] = True
+
+        max_dim = (0,0)
         for row in range(len(s)):
-            for col in range(len(s)):
-                if row==col:
-                    dpmat[row][col] = True
-                if col==row+1 and s[row]==s[col]:
-                    dparr[row][col] = True
+            for col in range(row, len(s)):
+
+                if st_mat[row][col]:
+                    if col-row>max_dim[1]-max_dim[0]:
+                        max_dim = (row,col)
 
 
-        max_coords = (0,0)
-        for row in range(len(s)-1,-1,-1):
-            for col in range(row+1, len(s)):
-
-                if dparr[row+1][col-1] and s[row]==s[col]:
-                    dparr[row][col] = True
-
-                if dparr[row][col]:
-                    if col-row>max_coords[1]-max_coords[0]:
-                        max_coords = (row,col)
-
-        return s[max_coords[0]:max_coords[1]+1]
+        return s[max_dim[0]:max_dim[1]+1]
