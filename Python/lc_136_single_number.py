e777aab18bceb5c3bf6b40f60a069ba2036e34ae
cmattey <cmattey@dons.usfca.edu>
2019-09-01 19:51:34 -0700

    Solved LC136 by using XOR operation

---
+++ Python/lc_136_single_number.py
@@ -0,0 +1,12 @@
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        res  = nums[0]
+
+        for i in range(1, len(nums)):
+            res = res^(nums[i])
+
+        return res
