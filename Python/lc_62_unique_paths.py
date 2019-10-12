b2e2f3ac4025b9f1fdad98d7b739141d8e0e3e5e
cmattey <cmattey@dons.usfca.edu>
2019-10-11 23:31:35 -0700

    Updated LC62 solution with Linear space solution

--- Python/lc_62_unique_paths.py
+++ Python/lc_62_unique_paths.py
@@ -1,5 +1,19 @@
 # 62. Unique Paths
 # Time: O(m*n)
+# Space: O(n)
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        dp_arr = [1]*n
+
+        for row in range(1,m):
+            for col in range(1,n):
+                dp_arr[col] = dp_arr[col-1] + dp_arr[col]
+
+        return dp_arr[-1]
+
+# Time: O(m*n)
 # Space: O(m*n)
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
