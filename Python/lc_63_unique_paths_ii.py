204f8fe83cb46db5bec43847dd576036025681d1
cmattey <cmattey@dons.usfca.edu>
2019-10-11 23:32:05 -0700

    Updated LC63 solution with Linear space solution

--- Python/lc_63_unique_paths_ii.py
+++ Python/lc_63_unique_paths_ii.py
@@ -1,6 +1,36 @@
 # 63. Unique Paths II
+# Time: O(m*n)
+# Space: O(n)
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+
+        dp_arr = [0]*len(obstacleGrid[0])
+        if obstacleGrid[0][0]:
+            return 0
+
+        for index,val in enumerate(obstacleGrid[0]):
+            if val:
+                break
+            dp_arr[index] = 1
+
+        if len(obstacleGrid)>1:
+            dp_arr[0] = 0 if obstacleGrid[1][0] else 1
+
+        for row in range(1,len(obstacleGrid)):
+            dp_arr[0] = dp_arr[0]*(1-obstacleGrid[row][0])
+            for col in range(1,len(obstacleGrid[0])):
+                if obstacleGrid[row][col]==1:
+                    dp_arr[col] = 0
+                else:
+                    dp_arr[col] = dp_arr[col-1]+dp_arr[col]
+
+        return dp_arr[-1]
+
+
 # Time:O(size(obstacleGrid))
 # Space:O(size(obstacelGrid))
+
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
 
