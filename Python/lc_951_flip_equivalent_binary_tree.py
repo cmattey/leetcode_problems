dd151b3c1ff25d4ad2a1ce0e561d7295ad1c0ed6
cmattey <cmattey@dons.usfca.edu>
2019-10-15 00:16:42 -0700

    Solved LC951 using recursion, checking root values and initiating 4 recursive calls, 2 for no_swap right and left child and 2 for swap

---
+++ Python/lc_951_flip_equivalent_binary_tree.py
@@ -0,0 +1,23 @@
+# If non-unique value: Time: 2^n
+# Time: O(n), since we return as soon as mismatch
+# Space: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+
+        if not root1 or not root2:
+            return root1==root2
+
+        else:
+            return root1.val==root2.val and \
+            (self.flipEquiv(root1.left,root2.left) and \
+             self.flipEquiv(root1.right,root2.right) or \
+             self.flipEquiv(root1.left, root2.right) and \
+             self.flipEquiv(root1.right,root2.left))
