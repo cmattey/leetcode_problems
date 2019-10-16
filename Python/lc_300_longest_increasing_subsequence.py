ddb71d1e819ea6ecccc1bd0e62670202d3b1d248
cmattey <cmattey@dons.usfca.edu>
2019-10-16 08:09:28 -0700

    Solved using DP array, O(n^2) time, can be done in O(nlogn) using binary search to find insertion indexin temp sequence

---
+++ Python/lc_300_longest_increasing_subsequence.py
@@ -0,0 +1,31 @@
+# Time: O(n^2)
+# Space: O(n)
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+
+        if not nums:
+            return 0
+        cur_max = None
+        overall_max = None
+
+        dp_arr = [0]*len(nums)
+        for index, num in enumerate(nums):
+            if index==0:
+                dp_arr[index] = 1
+            else:
+                start = index-1
+                cur_max = 0
+                while start>-1:
+                    if num>nums[start]:
+                        cur_max = max(cur_max,dp_arr[start]+1)
+                    start-=1
+
+                dp_arr[index] = cur_max if cur_max else 1
+
+        # print(dp_arr)
+        return max(dp_arr)
+
+
+
+        
