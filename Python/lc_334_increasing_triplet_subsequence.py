47c32399a45eb0c76ab81c1a9fa9912b65262b2c
cmattey <cmattey@dons.usfca.edu>
2019-10-17 21:43:35 -0700

    Solved LC334 using two variables to store first,second element of sequence

---
+++ Python/lc_334_increasing_triplet_subsequence.py
@@ -0,0 +1,29 @@
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+
+        if len(nums)<3:
+            return False
+
+        first = nums[0]
+        second = '#'
+
+        for num in nums[1:]:
+
+            if num==first:
+                continue
+
+            if num<first:
+                first = num
+
+            elif second=='#':
+                first = min(num,first)
+                second = max(num,first)
+            elif num<second:
+                second = num
+            elif num>second:
+                return True
+
+        return False
