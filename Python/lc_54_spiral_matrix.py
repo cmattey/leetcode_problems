8bf9ef41ba1203d9665bf4305a0ddc0c13c6276d
cmattey <cmattey@dons.usfca.edu>
2019-10-25 22:09:45 -0700

    Updated LC54 using conside code using arr.extend

--- Python/lc_54_spiral_matrix.py
+++ Python/lc_54_spiral_matrix.py
@@ -1,4 +1,39 @@
 # 54. Spiral Matrix
+
+# Time: O(size(mat))
+# Space: O(1), except output array
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        """
+        Shorter Code, with less code repetition
+        """
+        if not matrix or not matrix[0]:
+            return []
+
+        l,r = 0, len(matrix[0])-1
+        u,d = 0, len(matrix)-1
+
+        ans = []
+
+        while l<r and u<d:
+            ans.extend([matrix[u][col] for col in range(l,r)])
+            ans.extend([matrix[row][r] for row in range(u,d)])
+            ans.extend([matrix[d][col] for col in range(r,l,-1)])
+            ans.extend([matrix[row][l] for row in range(d,u,-1)])
+
+            l,r= l+1,r-1
+            u,d = u+1,d-1
+
+
+        if l==r:
+            ans.extend([matrix[row][l] for row in range(u,d+1)])
+        elif u==d:
+            ans.extend([matrix[u][col] for col in range(l,r+1)])
+
+        return ans
+
+
 # Time: O(size(matrix))
 # Space: O(1)
 class Solution:
