6cdf135d782a6618320e023ff506692bdbfedb8a
cmattey <cmattey@dons.usfca.edu>
2019-10-26 21:46:42 -0700

    Updated sol to LC1110 using cleaner pre-order traversal

--- Python/lc_1110_del_node_ret_forest.py
+++ Python/lc_1110_del_node_ret_forest.py
@@ -1,4 +1,40 @@
 # 1110. Delete Nodes And Return Forest
+
+# Time: O(n)
+# Space: O(n)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
+
+        forest = []
+        to_delete = set(to_delete)
+        self.preorder(root, None, forest, to_delete)
+        return forest
+
+    def preorder(self, root, prev, forest, to_delete):
+        if not root:
+            return
+
+        if not prev:
+            if root.val not in to_delete:
+                forest.append(root)
+
+        temp_left = root.left
+        temp_right = root.right
+        if root.right and root.right.val in to_delete:
+            root.right = None
+        if root.left and root.left.val in to_delete:
+            root.left = None
+
+        self.preorder(temp_left, root if root.val not in to_delete else None, forest, to_delete)
+        self.preorder(temp_right, root if root.val not in to_delete else None, forest, to_delete)
+
 # Weekly Contest 144
 # Time: O(len(TreeNode))
 # Space: O(len(TreeNode)) on recursion stack
