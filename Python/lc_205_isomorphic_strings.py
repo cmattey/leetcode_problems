a1b3e8b4a606b06e93c94008e3a5b25596f76736
cmattey <cmattey@dons.usfca.edu>
2019-10-26 21:47:48 -0700

    Updated sol to LC205, using map, seen set

--- Python/lc_205_isomorphic_strings.py
+++ Python/lc_205_isomorphic_strings.py
@@ -1,5 +1,32 @@
+# Time: O(s)
+# Space: O(t+s)
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+
+        imap = {}
+        already_mapped = set()
+
+
+        for index, ch in enumerate(s):
+
+            if s[index] not in imap:
+                if t[index] in already_mapped:
+                    return False
+
+                imap[s[index]] = t[index]
+                already_mapped.add(t[index])
+
+            if s[index] in imap:
+                if t[index] != imap[s[index]]:
+                    return False
+
+        return True
+                
+
 # Time: len(s)
-# Space: O(1)
+# Space: O(s+t)
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
@@ -17,5 +44,3 @@ class Solution:
                 return False
 
         return True
-
-        
