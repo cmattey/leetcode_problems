929e88351d350f0b5794d575e29fc7255d17c7e6
cmattey <cmattey@dons.usfca.edu>
2019-10-27 23:14:47 -0700

    Updated sol to LC939, both solutions included, using sweep approach

--- Python/lc_939_min_area_rectangle.py
+++ Python/lc_939_min_area_rectangle.py
@@ -1,3 +1,32 @@
+# Oct 27th '19
+# Time: O(n^2)
+# Space: O(n)
+class Solution:
+    def minAreaRect(self, points: List[List[int]]) -> int:
+
+        min_area = float('inf')
+
+        points.sort(key=lambda pt:pt[0])
+
+        left_edges = {}
+
+        for x1,y1 in points:
+            for x2, y2 in points:
+                if (x1==x2 and y1==y2) or y1<=y2:
+                    continue
+
+                if x1==x2:
+                    if (y1,y2) not in left_edges:
+                        left_edges[(y1,y2)] = x1
+                    else:
+                        x_dist = abs(x2-left_edges[(y1,y2)])
+                        y_dist = y1-y2
+                        min_area = min(min_area, x_dist*y_dist)
+                        left_edges[(y1,y2)] = x1
+
+        return min_area if min_area!=float('inf') else 0
+
+
 # Time: O(n^2)
 # Space: O(n)
 
