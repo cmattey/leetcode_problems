5bdfa833bf636722dfd9cb6d8482ebb909cf6f23
cmattey <cmattey@dons.usfca.edu>
2019-11-02 23:56:11 -0700

    Updated LC253, with heap logic

--- Python/lc_253_meeting_rooms_ii.py
+++ Python/lc_253_meeting_rooms_ii.py
@@ -23,4 +23,28 @@ class Solution:
         return len(heap)
 
 
-                    
+# Oct 28th '19
+# Time: O(nlogn)
+# Space: O(n)
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        import heapq
+
+        if not intervals:
+            return 0
+
+        intervals.sort(key=lambda kv:kv[0])
+
+        heap = [intervals[0][1]]
+
+        for start, end in intervals[1:]:
+
+            prev_end = heapq.heappop(heap)
+            if start<prev_end:
+                heapq.heappush(heap, prev_end)
+                heapq.heappush(heap, end)
+            else:
+                heapq.heappush(heap, max(prev_end,end))
+
+        return len(heap)
