540515a6244648ea7ddf1cb6b2c0bd7378ac665d
cmattey <cmattey@dons.usfca.edu>
2019-11-03 00:01:01 -0700

    Updated LC659 with early False return when condition not met

--- Python/lc_659_split_array_consecutive_subsequence.py
+++ Python/lc_659_split_array_consecutive_subsequence.py
@@ -1,3 +1,28 @@
+# Oct 29th '19
+# Time: O(n)
+# Space: O(unique(n))
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+
+        count_map = collections.defaultdict(int)
+        end_map = collections.defaultdict(int)
+
+        for num in nums:
+
+            if count_map[num]==0 and end_map[num-1]==0:
+                count_map[num+1]+=1
+                count_map[num+2]+=1
+                end_map[num+2]+=1
+            elif count_map[num]>0:
+                count_map[num]-=1
+            elif end_map[num-1]>0:
+                end_map[num-1]-=1
+                end_map[num]+=1
+            else:
+                return False
+
+        return not any(val for val in count_map.values())
+
 # Time: O(n)
 # Space: O(n)
 
@@ -23,4 +48,3 @@ class Solution:
                 end_map[num+2]+=1
 
         return not any(num_map.values())
-            
