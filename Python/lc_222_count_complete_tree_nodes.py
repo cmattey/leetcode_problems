a9f844dfb197060df56b443e9ba502f2698ac060
cmattey <cmattey@dons.usfca.edu>
2019-11-03 17:59:00 -0800

    Added iterative solution to LC222

--- Python/lc_222_count_complete_tree_nodes.py
+++ Python/lc_222_count_complete_tree_nodes.py
@@ -1,3 +1,51 @@
+# Nov 3rd '19
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Time: O(log^2n), each iterations does logn to find height, the recursion is breaking down a problem of size n, into n/2, by discarding half the tree, so T(n) = T(n/2)+k = logn Time
+# So logn time each for logn iterations = log^2(n)
+# Space: O(1)
+class Solution:
+    """
+    Iterative version
+    """
+    def countNodes(self, root: TreeNode) -> int:
+
+        if not root:
+            return 0
+
+        count = 0
+
+        while root:
+            left_depth = self.depth(root.left)
+            right_depth = self.depth(root.right)
+
+            if left_depth==right_depth:
+                count+= (2**(left_depth)-1)+1
+                root = root.right
+
+            else:
+                count+= (2**(right_depth)-1)+1
+                root = root.left
+
+        return count
+
+    def depth(self, root):
+
+        if not root:
+            return 0
+
+        depth = 0
+        while root:
+            depth+=1
+            root = root.left
+
+        return depth
+
 # Time: O((logn)**2)  logn height calculation, at each node at from top to bot, which is again logn node traversals
 # Space: O(1)
 
