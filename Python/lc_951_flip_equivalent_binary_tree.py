bb3ec04f504647e4953e2e59c779d3d301de0bcc
cmattey <cmattey@dons.usfca.edu>
2019-11-03 18:04:17 -0800

    Added a similar solution, using recursion

--- Python/lc_951_flip_equivalent_binary_tree.py
+++ Python/lc_951_flip_equivalent_binary_tree.py
@@ -1,3 +1,29 @@
+# Oct 28th '19
+# Time: O(n), min(both trees)
+# Space: O(h),min(heights) recursive stack
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
+
+        if not root1 and not root2:
+            return True
+
+        if (root1 and not root2) or (root2 and not root1):
+            return False
+
+
+        if root1.val==root2.val:
+            return (self.flipEquiv(root1.left,root2.left) and self.flipEquiv(root1.right,root2.right)) or self.flipEquiv(root1.left,root2.right) and self.flipEquiv(root1.right,root2.left)
+        else:
+            return False
+
 # If non-unique value: Time: 2^n
 # Time: O(n), since we return as soon as mismatch
 # Space: O(n)
