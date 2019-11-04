db01b54e6efd8b88ebb7dba3f5073210c3f3585d
cmattey <cmattey@dons.usfca.edu>
2019-11-03 18:00:03 -0800

    Cleaned solution using 2 vars to store smallest, and second smallest

--- Python/lc_334_increasing_triplet_subsequence.py
+++ Python/lc_334_increasing_triplet_subsequence.py
@@ -1,3 +1,27 @@
+# Oct28th '19
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+
+        if len(nums)<3:
+            return False
+
+        first = nums[0]
+        second = None
+
+        for num in nums[1:]:
+
+            if num<=first:
+                first = num
+            elif second==None or num<=second:
+                second = num
+            else:
+                return True
+
+        return False
+
+
 # Time: O(n)
 # Space: O(1)
 
