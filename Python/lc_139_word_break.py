0d3792474fe0f8872c8f803ab29e28bb822547c0
cmattey <cmattey@dons.usfca.edu>
2019-11-25 08:59:32 -0800

    Updated solution with comments

--- Python/lc_139_word_break.py
+++ Python/lc_139_word_break.py
@@ -1,4 +1,31 @@
 # 139. Word Break
+# Nov 22nd '19
+# Time: O(n^2), O(n^3), if we consider splicing?
+# Space: O(n)
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """
+        dp approach:
+
+        dp[i]: True if s[:i] can be broken down in to words that exist in wordDict
+
+        dp[i] = True if s[:i] in wordDict or for some k in [0,i], s[:k],s[k:j] in wordDict
+
+        Need to find if dp[0:len(s)] is True
+        """
+
+        dp = [True] + [False]*len(s)
+        for end in range(len(s)):
+            for start in range(end+1):
+                if dp[end+1]:
+                    continue
+
+                if s[start:end+1] in wordDict:
+                    if dp[start]:
+                        dp[end+1] = True
+
+        return dp[-1]
+
 # Time: O(len(s)^3)
 # Space: O(len(s))
 
