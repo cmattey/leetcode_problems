9284a9febd63f110c3f92b26e7a5d5031831feee
cmattey <cmattey@dons.usfca.edu>
2019-11-28 16:22:08 -0800

    Updated LC815 with BFS, and novel graph representation

--- Python/lc_815_bus_routes.py
+++ Python/lc_815_bus_routes.py
@@ -1,3 +1,48 @@
+# Nov 28th '19
+# Time: O(sum(N*bi)), where N= num_busese = len(routes), bi = stops on bus_i
+# Space: O(N(^2?) + sum(b_i)), N^2, cause in BFS, we can go from every stop to every other stop?
+
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], S: int, T: int) -> int:
+        """
+        Need to take least number of buses from source to target.
+
+        Each bus is represented as a node in the graph.
+        From this node, adjacent nodes are all buses with a common stop with
+        current bus_route.
+        """
+
+        bus_map = collections.defaultdict(set)
+
+        for index, route in enumerate(routes):
+            for stop in route:
+                bus_map[index].add(stop)
+
+
+        stop_map = collections.defaultdict(list)  # map to store stop->bus
+
+        for bus_num, route in enumerate(routes):
+            for stop in route:
+                stop_map[stop].append(bus_num)
+
+        seen_buses = set()
+        q = [(S, 0)]
+
+        while q:
+            cur_stop, bus_count = q.pop(0)
+
+            if cur_stop==T:
+                return bus_count
+#             for cur_stop, find all buses from it
+
+            for bus in stop_map[cur_stop]:
+                if bus not in seen_buses:
+                    for stop in bus_map[bus]:
+                        q.append((stop, bus_count+1))
+                    seen_buses.add(bus)
+
+        return -1
+
 # Time: O(N(^2?)+num_stops), where N=len(routes) <--Review
 # Space: O(N+num_stops), where N=len(routes)
 
