3c66c061775cb895051b8f228bcdc42b40caa8ca
cmattey <cmattey@dons.usfca.edu>
2019-12-01 17:06:43 -0800

    Updated solution to LC1055, same logic as using find, with start location parameter

--- Python/lc_1055_shortest_way_form_string.py
+++ Python/lc_1055_shortest_way_form_string.py
@@ -1,3 +1,55 @@
+# Nov 15th '19
+# Time: O(src*target)
+# Space: O(1)
+
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+
+        if len(set(source)&set(target)) < len(set(target)):
+            return -1
+
+        start_index = 0
+        count = 1
+        for ch in target:
+
+            start_index = source.find(ch, start_index)
+
+            if start_index==-1:
+                start_index = source.find(ch)
+                count+=1
+
+            start_index+=1
+
+        return count
+
+# Nov 11th '19
+# Time: O(src*target)
+# Space: O(1)
+
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+
+        min_count = 1
+
+        if len(set(source)&set(target)) < len(set(target)):
+            return -1
+
+        cur_loc = 0
+        t_index = 0
+        while t_index<len(target):
+
+            find_index = source.find(target[t_index], cur_loc)
+
+            if find_index!=-1:
+                cur_loc = find_index+1
+                t_index+=1
+            elif find_index==-1:
+                cur_loc = 0
+                min_count+=1
+
+        return min_count
+
+
 # Time: O(src*target), because of search
 # Space: O(1), if not accounting for set creation at beginning.
 
