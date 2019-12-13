0bb2ec7119c62b2b3bf197e26a88c57f3756d002
cmattey <cmattey@dons.usfca.edu>
2019-12-13 09:54:59 -0800

    Updated LC34 first last position of element, with cleaner Binary search with is_left parameter

--- Python/lc_34.py
+++ Python/lc_34.py
@@ -1,5 +1,105 @@
 # 34. Find First and Last Position of Element in Sorted Array
 
+# Nov 20th '19
+# Time: O(logn)
+# Space: O(1)
+
+class Solution:
+    """
+    Using Errichto Binary Search
+    """
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+
+        left_pos = self.bin_search(nums, target, True)
+
+        if left_pos==-1:
+            return [-1,-1]
+
+        right_pos = self.bin_search(nums, target, False)
+
+        return [left_pos, right_pos]
+
+
+    def bin_search(self, nums, target, is_left):
+
+        left = 0
+        right = len(nums)-1
+
+        ans = -1
+        while left<=right:
+
+            mid = (left+right)//2
+
+            if is_left:
+                if nums[mid]==target:
+                    ans = mid
+                    right = mid-1
+                elif nums[mid]<target :
+                    left = mid+1
+                else:
+                    right = mid-1
+            else:
+                if nums[mid]==target:
+                    ans = mid
+                    left = mid+1
+                elif nums[mid]>target:
+                    right = mid-1
+                else:
+                    left = mid+1
+
+        return ans
+
+
+
+# Nov 5th '19
+# Time: O(logn)
+# Space: O(1)
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+
+        if not nums:
+            return [-1,-1]
+
+        low = 0
+        high = len(nums)-1
+
+#         Left-Bound
+        while low<high:
+            mid = (low+high)//2
+
+            if nums[mid]==target:
+                high = mid
+            elif nums[mid]>target:
+                high = mid-1
+            else:
+                low = mid+1
+
+        left = high
+
+        if nums[left]!=target:
+            return [-1,-1]
+
+        low = 0
+        high = len(nums)-1
+
+        while low<high:
+
+            mid = (low+high)//2 +1  # Key is to make this mid, right-biased
+
+            if nums[mid]==target:
+                low = mid
+            elif nums[mid]<target:
+                low = mid+1
+            else:
+                high = mid-1
+
+        right = low
+
+        if nums[right]!=target:
+            return [-1,-1]
+
+        return [left,right]
+
 # Nov 1st '19
 # Time: O(logn)
 # Space: O(1)
