31c4af6c34ec395c59885133dde224fd0795ab71
cmattey <cmattey@dons.usfca.edu>
2019-12-14 21:15:59 -0800

    Updated LC210 with topological sort solution

--- Python/lc_210_course_schedule_ii.py
+++ Python/lc_210_course_schedule_ii.py
@@ -1,3 +1,72 @@
+# Nov 16th '19
+# Time: O(numCourses)
+# Space: O(numCourses)
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        """
+        Topological Sort Solution
+        """
+
+        dep_map = collections.defaultdict(list)
+        in_degree = collections.defaultdict(int)
+
+        for dep, course in prerequisites:
+            dep_map[course].append(dep)
+            in_degree[dep]+=1
+
+        # print(in_degree)
+        indies = [course for course in range(numCourses) if course not in in_degree]
+        # print(indies)
+        order = []
+        while indies:
+            cur_course = indies.pop(0)
+            # print(cur_course)
+            order.append(cur_course)
+
+            for course in dep_map[cur_course]:
+                in_degree[course]-=1
+                if in_degree[course]==0:
+                    indies.append(course)
+
+        return order if len(order)==numCourses else []
+
+
+        """
+        DFS solution
+        """
+
+        dep_map = collections.defaultdict(list)
+
+        for dep, course in prerequisites:
+            dep_map[dep].append(course)
+
+        visited = ["unvisited" for _ in range(numCourses)]
+
+        order = []
+        for course in range(numCourses):
+
+            if not self.dfs(course, dep_map, order, visited):
+                return []
+
+        return order
+
+    def dfs(self, course, dep_map, order, visited):
+
+        if visited[course]=="in-progress":
+            return False
+        elif visited[course]=="done":
+            return True
+
+        visited[course]="in-progress"
+
+        for dep in dep_map[course]:
+            if not self.dfs(dep, dep_map, order, visited):
+                return False
+
+        order.append(course)
+        visited[course] = "done"
+        return True
+
 # 210. Course Schedule II
 # Time: O(V+E)
 # Space: O(V+E)
