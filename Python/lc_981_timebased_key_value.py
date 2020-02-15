c4a9662d675351a0ebd567b4d1f3a17fd23e2b45
cmattey <cmattey@dons.usfca.edu>
2020-02-14 21:52:00 -0800

    Added explicit binary search solution

--- Python/lc_981_timebased_key_value.py
+++ Python/lc_981_timebased_key_value.py
@@ -7,6 +7,50 @@ class TimeMap:
         """
         Initialize your data structure here.
         """
+        import collections
+
+        self.imap = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+
+        self.imap[key].append((timestamp, value))
+
+
+    def get(self, key: str, timestamp: int) -> str:
+
+        if key not in self.imap:
+            return ""
+
+        values = self.imap[key]
+
+        left = 0
+        right = len(values)-1
+
+        ans = ""
+        while left<=right:
+
+            mid = (left+right)//2
+
+            if values[mid][0]<=timestamp:
+                ans = values[mid][1]
+                left = mid+1
+            else:
+                right = mid-1
+
+        return ans
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+
+class TimeMap:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
         self.imap = {}
 
 
