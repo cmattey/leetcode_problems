6725866424db535a64310a1e52b349e8ccd4210b
cmattey <cmattey@dons.usfca.edu>
2020-03-09 23:46:46 -0700

    updated review, solved using hashmap to store location, and find substrings where each chr occurs only once

--- Python/lc_828_count_unqiue_chars_substrings.py
+++ Python/lc_828_count_unqiue_chars_substrings.py
@@ -1,3 +1,26 @@
+# Review Mar 9th '20'
+# Time: O(N)
+# Space: O(N)
+class Solution:
+    def uniqueLetterString(self, s: str) -> int:
+
+        import collections
+
+        ch_map = collections.defaultdict(list)
+
+        for index, ch in enumerate(s):
+            ch_map[ch].append(index)
+
+        total_count = 0
+
+        for ch, pos in ch_map.items():
+            pos = [-1]+pos+[len(s)]
+
+            for i in range(1,len(pos)-1):
+                total_count += (pos[i]-pos[i-1])*(pos[i+1]-pos[i])
+
+        return total_count%(10**9 + 7)
+
 # Time: O(N), max chars 52
 # Space: O(N)
 class Solution:
