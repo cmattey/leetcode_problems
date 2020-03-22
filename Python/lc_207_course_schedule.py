fb0805ededcc1b206fe30d6bbb003b141f942037
cmattey <cmattey@dons.usfca.edu>
2020-03-21 23:59:10 -0700

    updated solution using topological sort

--- Python/lc_207_course_schedule.py
+++ Python/lc_207_course_schedule.py
@@ -1,4 +1,36 @@
 # 207. Course Schedule
+# Time: 2E + V
+# Space: E + 2V
+# Topological Sort
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        import collections
+
+        dep_map = collections.defaultdict(list)
+        dep_count = collections.defaultdict(int)
+
+        for dep, course in prerequisites:
+            dep_map[course].append(dep)
+            dep_count[dep]+=1
+
+        indies = [course for course in range(numCourses) if course not in dep_count]
+
+        indie_count = 0
+
+        while indies:
+
+            cur_course = indies.pop(0)
+
+            for course in dep_map[cur_course]:
+                dep_count[course]-=1
+                if dep_count[course]==0:
+                    del dep_count[course]
+                    indies.append(course)
+
+        if len(dep_count)>0:
+            return False
+        return True
+
 # Time: O(V+E) vertices + edges
 # Space: O(V+E)
 class Solution:
