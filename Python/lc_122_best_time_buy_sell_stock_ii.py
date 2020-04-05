758de12551f6d85a9006ddfbc48f1f4369987f14
cmattey <cmattey@dons.usfca.edu>
2020-04-05 15:51:30 -0700

    updated solution with clearner logic

--- Python/lc_122_best_time_buy_sell_stock_ii.py
+++ Python/lc_122_best_time_buy_sell_stock_ii.py
@@ -1,7 +1,38 @@
 # Time: O(n), where n = len(prices)
 # Space: O(1)
 
+# April 5th
+
+# Time: O(n)
+# Space: O(1)
+
 class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        nums = prices
+
+        profit = 0
+
+        start, end = 0,1
+
+        while end<len(nums):
+
+            if nums[start]>=nums[end]:
+                start = end
+                end +=1
+            else:
+                while end+1<len(nums) and nums[end]<nums[end+1]:
+                    end+=1
+
+                profit+=nums[end]-nums[start]
+
+                start = end
+                end +=1
+
+        return profit
+
+
+class Solution2:
     def maxProfit(self, prices: List[int]) -> int:
 
         profit = 0
@@ -21,6 +52,3 @@ class Solution:
             index+=1
 
         return profit
-
-
-        
