40ad213bef53b7f0aae71df3cb8a16b12b82ac24
Chaitanya Mattey <cmattey94@gmail.com>
2020-05-04 22:20:34 -0700

    use map to store prev occurence, and maintain start position

---
+++ Go/lc_3_longest_substr_no_repeating_chars.go
@@ -0,0 +1,36 @@
+# Time: O(n)
+# Space: O(n)
+import (
+    "fmt"
+)
+func lengthOfLongestSubstring(s string) int {
+
+    maxLength := 0
+    start := 0
+
+    imap := make(map[string]int)
+
+    for index, ch := range s{
+
+        pos, ok := imap[string(ch)]
+        if !ok {
+            imap[string(ch)] = index
+            maxLength = Max(maxLength, index-start+1)
+        } else if ok && pos >= start {
+            start = pos+1
+            imap[string(ch)] = index
+        } else {
+            imap[string(ch)] = index
+            maxLength = Max(maxLength, index-start+1)
+        }
+    }
+
+    return maxLength
+}
+
+func Max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
