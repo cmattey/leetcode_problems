f74cd92925f493ec16491550babf0c9f464cfacf
Chaitanya Mattey <cmattey94@gmail.com>
2020-05-17 21:16:02 -0700

    LC 7: mod 10 to reverse number
    - Check for integer overflow lazily (after data type has been promoted)

---
+++ Go/lc_7_reverse_integer.go
@@ -0,0 +1,30 @@
+ // Time: O(logn) base 10
+ // Space: O(1)
+func reverse(x int) int {
+    isNeg := false
+    if x<0{
+        isNeg = true
+        x = -x
+    }
+
+    ans := 0
+    for x>0{
+        ans = ans*10 + x%10
+        x = x/10
+
+    }
+
+    if isNeg{
+        ans = -ans
+        if ans<(-1*(1<<31)){
+            return 0
+        }
+        return ans
+    }
+
+    if ans>1<<31{
+        return 0
+    }
+    return ans
+
+}
