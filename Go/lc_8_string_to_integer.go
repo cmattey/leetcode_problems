071de001487dc047d2faf5b4b7d9ad9699b6294b
Chaitanya Mattey <cmattey94@gmail.com>
2020-06-29 21:25:03 -0700

    LC-8: Use strings, strconv packages. TrimSpace func.
    - Iterate over string and join integers and convert
    - Room for improvement, to try and avoid multiple type conversions

---
+++ Go/lc_8_string_to_integer.go
@@ -0,0 +1,42 @@
+import(
+    "strings"
+    "strconv"
+)
+func myAtoi(str string) int {
+
+    str = strings.TrimSpace(str)
+
+    if len(str)==0{
+        return 0
+    }
+
+    var output []string
+
+    if str[0] == '-' || str[0]=='+'{
+        output = append(output, string(str[0]))
+        str = str[1:]
+    }
+
+    nums := "0123456789"
+
+    for _,ch := range str {
+        if ch == rune(' ') || !strings.Contains(nums, string(ch)){
+            break
+        }
+
+        output = append(output, string(ch))
+
+    }
+
+    o, _ := strconv.ParseInt(strings.Join(output,""), 10, 64)
+
+    if o<(-1*(1<<31)){
+        return -1*1<<31
+    }
+
+    if o>1<<31-1{
+        return 1<<31-1
+    }
+
+    return int(o)
+}
