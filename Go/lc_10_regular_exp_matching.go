a0093482d1c43fabdfa1472943fe8ac4d845a86e
Chaitanya Mattey <cmattey94@gmail.com>
2020-06-30 20:33:17 -0700

    LC 10: Use DP. dp(i,j) represents whether substring s[i:], matches pattern p[j:]

---
+++ Go/lc_10_regular_exp_matching.go
@@ -0,0 +1,53 @@
+package Go
+
+import (
+	"fmt"
+)
+
+func isMatch(s string, p string) bool {
+
+	m := make(map[Pair]bool)
+
+	return dp(0, 0, m, s, p)
+}
+
+func dp(i int, j int, m map[Pair]bool, s string, p string) bool {
+
+	var ans bool
+	val, ok := m[Pair{i, j}]
+
+	if ok {
+		return val
+	}
+
+	if len(p) == j {
+		m[Pair{i, j}] = i == len(s)
+		return m[Pair{i, j}]
+	}
+
+	first_match := i < len(s) && contains([]string{".", string(s[i])}, string(p[j]))
+
+	if j+1 < len(p) && string(p[j+1]) == "*" {
+		ans = dp(i, j+2, m, s, p) || first_match && dp(i+1, j, m, s, p)
+	} else {
+		ans = first_match && dp(i+1, j+1, m, s, p)
+	}
+
+	m[Pair{i, j}] = ans
+
+	return m[Pair{i, j}]
+}
+
+func contains(sl []string, s string) bool {
+	for _, v := range sl {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+type Pair struct {
+	a int
+	b int
+}
