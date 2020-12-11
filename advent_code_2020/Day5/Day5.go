d0d5c54edc769f1cf5c1fa91c6cb43b8b7709429
Chaitanya Mattey <cmattey94@gmail.com>
2020-12-10 21:46:28 -0700

    Solve Day5 Part2

--- advent_code_2020/Day5/Day5.go
+++ advent_code_2020/Day5/Day5.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Println(solveDay5("Day5_input.txt"))
+	fmt.Println(solveDay5Part2("Day5_input.txt"))
 	// fmt.Println(solveDay5("test_input.txt"))
 }
 
@@ -51,4 +52,62 @@ func solveDay5(filename string) int {
 	}
 
 	return ans
+}
+
+func solveDay5Part2(filename string) int {
+
+	strs := helpers.ReadStr(filename)
+
+	seatingChart := [128][8]int{}
+
+	ans := 0
+	for _, st := range strs {
+
+		low, high := 0, 127
+		left, right := 0, 7
+		r, c := 0, 0
+		for i, val := range st {
+			if i < 7 {
+				if string(val) == "F" {
+					r = int((low + high) / 2)
+					high = r
+				} else {
+					r = int((low+high)/2) + 1
+					low = r
+				}
+			} else {
+				if string(val) == "L" {
+					c = int((left + right) / 2)
+					right = c
+				} else {
+					c = int((left+right)/2) + 1
+					left = c
+				}
+			}
+		}
+
+		seatingChart[r][c] = 1
+		curAns := r*8 + c
+		ans = helpers.Max(ans, curAns)
+
+	}
+
+	r, c := findMySeat(seatingChart)
+	return r*8 + c
+}
+
+func findMySeat(matrix [128][8]int) (int, int) {
+	firstNonEmptyFound := false
+	for r := range matrix {
+		for c := range matrix[0] {
+
+			if firstNonEmptyFound && matrix[r][c] == 0 {
+				return r, c
+			}
+			if matrix[r][c] == 1 {
+				firstNonEmptyFound = true
+			}
+		}
+	}
+	return -1, -1
 }
