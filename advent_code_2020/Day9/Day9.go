c211087c187db15913c783005de8de3e78224034
Chaitanya Mattey <cmattey94@gmail.com>
2020-12-14 16:29:05 -0700

    interim sol to Part2

--- advent_code_2020/Day9/Day9.go
+++ advent_code_2020/Day9/Day9.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
-	fmt.Println(solveDay9Part1("Day9_input.txt"))
+	// nums := helpers.ReadNums("Day9_input.txt")
+	testNums := helpers.ReadNums("Day9_input_test.txt")
+	// fmt.Println(solveDay9Part1(nums))
+	// fmt.Println(solveDay9Part2(nums))
+	fmt.Println(solveDay9Part2(testNums))
 	// fmt.Println(solveDay9Part1("Day9_input_test.txt"))
 }
 
-func solveDay9Part1(filename string) int {
-	preambleSize := 25
-	// preambleSize := 5
-	nums := helpers.ReadNums(filename)
-
+func solveDay9Part1(nums []int) int {
+	// preambleSize := 25
+	preambleSize := 5
 	numMap := make(map[int]int)
 	for _, v := range nums[:preambleSize] {
 		numMap[v]++
@@ -38,6 +40,34 @@ func solveDay9Part1(filename string) int {
 			delete(numMap, numToRemove)
 		}
 		numMap[num]++
+	}
+
+	return -1
+}
+
+func solveDay9Part2(nums []int) int {
+
+	target := solveDay9Part1(nums)
+
+	rangeStart := 0
+	index := rangeStart
+	numSlice := make([]int, 0)
+	curSum := 0
+	for index < len(nums) {
+		curSum += nums[rangeStart]
+		numSlice = append(numSlice, nums[index])
+		if curSum > target {
+			rangeStart++
+			index = rangeStart
+			curSum = 0
+			numSlice = make([]int, 0)
+		} else if curSum == target {
+			fmt.Println(numSlice)
+			break
+			// ans := computeAns(numSlice)
+		} else {
+			index++
+		}
 	}
 
 	return -1
